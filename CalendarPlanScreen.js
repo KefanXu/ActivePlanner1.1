f3c7690b0239b1b62b98761036c46adee21a2920
Kefan <40338496+KefanXu@users.noreply.github.com>
2021-03-14 20:54:46 -0400

    Update CalendarPlanScreen.js

---
+++ CalendarPlanScreen.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import { render } from "react-dom";
+import {
+  TextInput,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  Alert,
+  Modal,
+  LayoutAnimation,
+  Button,
+  Animated,
+} from "react-native";
+import { getDataModel } from "./DataModel";
+
+export class CalendarPlanScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    };
+  }
+
+  render() {
+    return(
+      true
+    );
+  }
+}
