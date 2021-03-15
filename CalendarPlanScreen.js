8b106ee2dc79c972c9dfa2cd406256f10fa3a001
Kefan <40338496+KefanXu@users.noreply.github.com>
2021-03-14 20:52:17 -0400

    Update CalendarPlanScreen.js

--- CalendarPlanScreen.js
+++ CalendarPlanScreen.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import { render } from "react-dom";
+<<<<<<< HEAD
 
+=======
+>>>>>>> parent of 4ff0deb (add google calendar api 1)
 import {
   TextInput,
   Text,
