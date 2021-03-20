835cfd4c02e8b24a51b217211d3f2a9e6e5df99c
Kefan <40338496+KefanXu@users.noreply.github.com>
2021-03-20 17:16:31 -0400

    add dayz package

--- CalendarPlanScreen.js
+++ CalendarPlanScreen.js
@@ -15,6 +15,37 @@ import {
 import { getDataModel } from "./DataModel";
 
 import * as Google from "expo-google-app-auth";
+import Dayz from 'dayz';
+
+
+import moment from 'moment';
+// would come from a network request in a "real" app
+
+const events = new Dayz.EventsCollection([
+                { content: 'Continuing event Past',
+                  range: moment.range(moment('2015-09-08'), moment('2015-09-14')) },
+
+                { content: 'Continuing event Before',
+                  range: moment.range('2015-09-04', '2015-09-09') },
+
+                { content: 'Weeklong',
+                  range: moment.range('2015-09-06', moment('2015-09-12').endOf('day')) },
+
+                { content: 'A Longer Event',
+                  range: moment.range(moment('2015-09-04'), moment('2015-09-14')) },
+
+                { content: 'Inclusive',
+                  range: moment.range(moment('2015-09-07'), moment('2015-09-12')) },
+
+                { content: '9am - 2pm (resizable)',
+                  resizable: { step: 15 },
+                  range: moment.range(moment('2015-09-11').hour(9),
+                                      moment('2015-09-11').hour(14)) },
+
+                { content: '8am - 8pm (non-resizable)',
+                  range: moment.range(moment('2015-09-07').hour(8),
+                                      moment('2015-09-07').hour(21).minutes(40)) },
+            ]);
 
 const config = {
   // clientId:
@@ -31,8 +62,10 @@ const config = {
 export class CalendarPlanScreen extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {};
     this.dataModel = getDataModel();
+    this.date = moment('2015-09-11');
     //this.googleServiceInit();
   }
   googleServiceInit = async () => {
@@ -97,6 +130,10 @@ export class CalendarPlanScreen extends React.Component {
           title="Push a notification in 5s" 
           onPress = {this.dataModel.scheduleNotification}>
         </Button>
+        <Dayz
+          date = {this.date}
+          display='week'
+      />
       </View>
     );
   }
