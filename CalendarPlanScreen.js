11eb566a9c1f1a264cca35a4c036c86e23327bcc
Kefan <40338496+KefanXu@users.noreply.github.com>
2021-03-20 17:29:28 -0400

    Revert "testing calendar layout @ericz1803/react-google-calendar"
    This reverts commit 2e29402cea7e2c77f25dd0cf7ed3fcc2579df495.

--- CalendarPlanScreen.js
+++ CalendarPlanScreen.js
@@ -15,9 +15,6 @@ import {
 import { getDataModel } from "./DataModel";
 
 import * as Google from "expo-google-app-auth";
-import * as Calendar from "@ericz1803/react-google-calendar";
-import { css } from "@emotion/react";
-
 
 const config = {
   // clientId:
