0c4c590223317e6c99f022b443b6bd1cf7bfde18
Kefan <40338496+KefanXu@users.noreply.github.com>
2021-04-01 10:19:17 -0400

    implement report

---
+++ steps/step1.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { Image, View, TouchableOpacity, TextInput, Text } from "react-native";
+
+class step1 extends Component {
+  constructor(props) {
+
+  }
+  render() {
+    
+  }
+
+}
