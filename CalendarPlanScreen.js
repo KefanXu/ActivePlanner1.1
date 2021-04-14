367975787c3ea0e0ca4723e5dd95b23c3c4a148c
Kefan <40338496+KefanXu@users.noreply.github.com>
2021-04-14 14:00:33 -0400

    implement picker - failed

--- CalendarPlanScreen.js
+++ CalendarPlanScreen.js
@@ -28,6 +28,8 @@ import moment, { min } from "moment";
 import { Ionicons } from "@expo/vector-icons";
 import { MaterialIcons } from "@expo/vector-icons";
 import SwitchSelector from "react-native-switch-selector";
+
+import { Picker } from "@react-native-picker/picker";
 // import AnimatedMultistep from "react-native-animated-multistep";
 // const reportOptions = [
 //   {
@@ -1372,7 +1374,7 @@ export class CalendarPlanScreen extends React.Component {
                 style={{
                   flex: 0.25,
                   width: "100%",
-                  height:"100%",
+                  height: "100%",
                   justifyContent: "flex-start",
                   // backgroundColor:"blue"
                 }}
@@ -1409,9 +1411,8 @@ export class CalendarPlanScreen extends React.Component {
                 style={{
                   flex: 0.25,
                   width: "100%",
-                  height:"100%",
+                  height: "100%",
                   justifyContent: "flex-start",
-                  
                 }}
               >
                 <Text
@@ -1439,11 +1440,22 @@ export class CalendarPlanScreen extends React.Component {
                 backgroundColor: "#6E6E6E",
               }}
             >
-              <Button
+              {/* <Button
                 title="Plan"
                 disabled={this.state.isPlanBtnDisable}
                 onPress={() => this.onPlanBtnPressed()}
-              ></Button>
+              ></Button> */}
+              <Picker
+                selectedValue={"java"}
+                onValueChange={(itemValue, itemIndex) =>
+                  console.log(itemValue)
+
+                }
+                style={{height:50, width:50}}
+              >
+                <Picker.Item label="Java" value="java" />
+                <Picker.Item label="JavaScript" value="js" />
+              </Picker>
             </View>
           </View>
         </SlidingUpPanel>
