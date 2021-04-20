051ca1bf7d1798e1e38df0ea4833e3ab13a0faac
Kefan <40338496+KefanXu@users.noreply.github.com>
2021-04-20 11:10:11 -0400

    remove Secret.js on git

--- Secret.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyBB9VHmfWrnGFhke_D0zln_O9wYlelR-Gw",
-    authDomain: "activeplanner.firebaseapp.com",
-    projectId: "activeplanner",
-    storageBucket: "activeplanner.appspot.com",
-    messagingSenderId: "858218224278",
-    appId: "1:858218224278:web:2baf2b07b2972e9f0840b7",
-    measurementId: "G-9K7BQF6PLH"
-  };
